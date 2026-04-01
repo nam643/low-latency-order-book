64a208663eacb57b42f516be59528d36e1e54441
Nam Pham <hainamail1710@gmail.com>
2026-04-01 13:40:32 +1100

    Update Event Reader

---
+++ include/io/EventReader.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Event.hpp"
+
+class EventReader{
+public:
+    explicit EventReader(const std::string& filename);
+
+    bool hasNext() const;
+    Event next();
+    std::vector<Event> readAll();
+}
