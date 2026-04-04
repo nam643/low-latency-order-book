5d82e61b8d89bdd8f7142e104a73577a6b66bb31
Nam Pham <hainamail1710@gmail.com>
2026-04-05 08:48:27 +1000

    Added one simple test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-#include <string>
+#include "../include/core/MatchingEngine.hpp"
+#include "../include/core/Order.hpp"
 
-int main(int argc, char* argv[]);
+int main(){
+    MatchingEngine engine;
 
-void runFromFile(const std::string& filenam);
+    Order sell1(1, Side::Sell, OrderType::Limit, 101, 10, 1);
+    Order buy1(2, Side::Buy, OrderType::Limit, 101, 6, 2);
 
-void runInteractiveMode();
+    ExecutionReport report1 = engine.processAddOrder(sell1);
+    std::cout << "Add sell1: " << report1.message << "\n";
+
+    ExecutionReport report2 = engine.processAddOrder(buy1);
+    std::cout << "Add buy1: " << report2.message << "\n";
+
+    if (report2.hasTrades()) {
+        std::cout << "Trades:\n";
+        for (const auto& trade : report2.trades) {
+            std::cout << trade.toString() << "\n";
+        }
+    } else {
+        std::cout << "No trades generated.\n";
+    }
+
+    std::cout << "\nFinal Order Book:\n";
+    std::cout << engine.getOrderBook().toString();
+
+    return 0;
+}
