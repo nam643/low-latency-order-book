a9edaeb377d3c0fb7106d6be66524fe62e33eb3a
Nam Pham <hainamail1710@gmail.com>
2026-04-06 18:17:34 +1000

    Updated CSVParser.hpp, remove duplicated functions

--- include/io/CSVParser.hpp
+++ include/io/CSVParser.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 //Reads a CSV row and turns it into an Event
 #include "io/Event.hpp"
 #include <vector>
@@ -8,12 +10,8 @@ public:
     Event parseLine(const std::string& line) const;
     std::vector<Event> parseFile(const std::string& filename) const;
 
-    std::vector<std::string> split(const std::string& line, char delimiter) const;
-    Side parseSide(const std::string& token) const;
-    OrderType parseOrderType(const std::string& token) const;
-    EventType parseEventType(const std::string& token) const;
 private:
-    std::vector<std::string> split(const std::string& line, char delimeter) const;
+    std::vector<std::string> split(const std::string& line, char delimiter) const;
     Side parseSide(const std::string& token) const;
     OrderType parseOrderType(const std::string& token) const;
     EventType parseEventType(const std::string& token) const;
