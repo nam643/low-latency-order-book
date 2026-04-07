f9ca863b0b153b7cb78f39626091b95b96715fdc
Nam Pham <hainamail1710@gmail.com>
2026-04-07 12:56:21 +1000

    Added missing private field

--- include/io/EventReader.hpp
+++ include/io/EventReader.hpp
@@ -1,13 +1,18 @@
+#pragma once
+
 #include <vector>
 #include <string>
-#include <iostream>
 #include "Event.hpp"
+#include "CSVParser.hpp"
 
 class EventReader{
+private:
+    std::vector<Event> events_;
+    std::size_t currentIndex_;
 public:
     explicit EventReader(const std::string& filename);
 
     bool hasNext() const;
     Event next();
-    std::vector<Event> readAll();
+    std::vector<Event> readAll() const;
 };
